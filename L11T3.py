952994c8f425f79338cb61991488efd7e3fb9c41
Hannes K <101644829+Hansk1@users.noreply.github.com>
2023-01-11 00:16:35 +0200

    Update L11T3.py

--- L11T3.py
+++ L11T3.py
@@ -1,18 +1,3 @@
-######################################################################
-# CT60A0203 Ohjelmoinnin perusteet
-# Tekijä: Hannes Karppinen
-# Opiskelijanumero: 491936
-# Päivämäärä: 26.11.2022
-# Kurssin oppimateriaalien lisäksi työhön ovat vaikuttaneet seuraavat
-# lähteet ja henkilöt, ja se näkyy tehtävässä seuraavalla tavalla:
-# ----
-# Mahdollisen vilppiselvityksen varalta vakuutan, että olen tehnyt itse
-# tämän tehtävän ja vain yllä mainitut henkilöt sekä lähteet ovat
-# vaikuttaneet siihen yllä mainituilla tavoilla.
-######################################################################
-# Tehtävä L11T3.py
-# eof
-
 def rekursiivinenFunktio(TulostettavaSana, TulostuskertojenMaara, Tulostettu = 1):
     if TulostuskertojenMaara > 0:
         print("Sana on '" + TulostettavaSana + "',", str(Tulostettu) + ". kerta.")
