be2263d921e8209711a3da46578de770ad7453ad
Hannes K <101644829+Hansk1@users.noreply.github.com>
2023-01-11 00:14:12 +0200

    Update L08T2.py

--- L08T2.py
+++ L08T2.py
@@ -1,18 +1,3 @@
-######################################################################
-# CT60A0203 Ohjelmoinnin perusteet
-# Tekijä: Hannes Karppinen
-# Opiskelijanumero: 491936
-# Päivämäärä: 7.11.2022
-# Kurssin oppimateriaalien lisäksi työhön ovat vaikuttaneet seuraavat
-# lähteet ja henkilöt, ja se näkyy tehtävässä seuraavalla tavalla:
-# ----
-# Mahdollisen vilppiselvityksen varalta vakuutan, että olen tehnyt itse
-# tämän tehtävän ja vain yllä mainitut henkilöt sekä lähteet ovat
-# vaikuttaneet siihen yllä mainituilla tavoilla.
-######################################################################
-# Tehtävä L08T2.py
-# eof
-
 import L08T2Kirjasto as kirjasto
 
 #Haetaan kirjastosta kirjaston versionumero ja tallennetaan se kiintoarvoksi
