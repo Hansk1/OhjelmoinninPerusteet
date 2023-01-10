574ff2692ddf8e86d43dd9af0e4ef01201239fa3
Hannes K <101644829+Hansk1@users.noreply.github.com>
2023-01-11 00:19:24 +0200

    Update L14T3.py

--- L14T3.py
+++ L14T3.py
@@ -1,17 +1,3 @@
-######################################################################
-# CT60A0203 Ohjelmoinnin perusteet
-# Tekijä: Hannes Karppinen
-# Opiskelijanumero: 491936
-# Päivämäärä: 19.12.2022
-# Kurssin oppimateriaalien lisäksi työhön ovat vaikuttaneet seuraavat
-# lähteet ja henkilöt, ja se näkyy tehtävässä seuraavalla tavalla:
-# ----
-# Mahdollisen vilppiselvityksen varalta vakuutan, että olen tehnyt itse
-# tämän tehtävän ja vain yllä mainitut henkilöt sekä lähteet ovat
-# vaikuttaneet siihen yllä mainituilla tavoilla.
-######################################################################
-# Tehtävä L14T3.py
-
 #Tuodaan kirjastot:
 import datetime
 
